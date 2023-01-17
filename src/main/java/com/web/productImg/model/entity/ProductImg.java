b83a1b3c8aef0d6010a1ec4973868225d05a2779
Alvis <q8956895@gmail.com>
2023-01-17 19:37:25 +0800

    create entity

---
+++ src/main/java/com/web/productImg/model/entity/ProductImg.java
@@ -0,0 +1,39 @@
+package com.web.productImg.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GeneratorType;
+
+import com.core.model.entities.Core;
+import com.web.product.model.entity.Product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(catalog = "IPETDB", name = "product_img")
+public class ProductImg extends Core{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "IMG_ID")
+	private Integer imgID;
+	@Column(name = "IMG_FILE")
+	private byte[] imgFile;
+	
+}
