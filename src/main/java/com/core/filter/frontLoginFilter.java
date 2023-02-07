dd1ca7fc1077a7f247c3f6aa90a9411141a8a53d
HarryJian <a5130401@gmail.com>
2023-02-07 16:45:56 +0800

    update filter about member

--- src/main/java/com/core/filter/frontLoginFilter.java
+++ src/main/java/com/core/filter/frontLoginFilter.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.web.member.model.entity.Member;
-
 @WebFilter({ "/ipet-front/salon/addAppointment" })
 public class frontLoginFilter extends HttpFilter {
 	private static final long serialVersionUID = 1L;
@@ -20,9 +18,9 @@ public class frontLoginFilter extends HttpFilter {
 	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		HttpSession session = req.getSession();
-		Member member = (Member) session.getAttribute("member");
-		
-		if (member.getMemId() != null) {
+		Object memId = session.getAttribute("member");
+
+		if (memId != null) {
 			chain.doFilter(req, res);
 		} else {
 			req.getRequestDispatcher("/templates/frontstage/member/login.jsp").forward(req, res);
