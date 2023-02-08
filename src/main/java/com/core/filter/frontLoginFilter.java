e0e80dfb4ebecf342029effbab147f6ea715d7cc
Lynn <lynn990908@gmail.com>
2023-02-08 13:16:55 +0800

    merge

--- src/main/java/com/core/filter/frontLoginFilter.java
+++ src/main/java/com/core/filter/frontLoginFilter.java
@@ -18,14 +18,9 @@ public class frontLoginFilter extends HttpFilter {
 	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		HttpSession session = req.getSession();
-<<<<<<< HEAD
-		Member member = (Member) session.getAttribute("member");
-		if (member.getMemId() != null) {
-=======
 		Object memId = session.getAttribute("member");
 
 		if (memId != null) {
->>>>>>> dd1ca7fc1077a7f247c3f6aa90a9411141a8a53d
 			chain.doFilter(req, res);
 		} else {
 			req.getRequestDispatcher("/templates/frontstage/member/login.jsp").forward(req, res);
